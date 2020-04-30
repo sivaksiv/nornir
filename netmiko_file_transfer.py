0d2086865a13126e4c285a20e0634b1481d599cc
Sivakumar Sukumar <sivaksiv@cisco.com>
2020-04-30 18:17:27 +0530

    test

---
+++ netmiko_file_transfer.py
@@ -0,0 +1,19 @@
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_file_transfer
+
+from nornir_utilities import nornir_set_creds, std_print
+
+
+# Initialize Nornir object using default "SimpleInventory" plugin
+nr = InitNornir()
+nornir_set_creds(nr)
+test_file = 'test_xfer.txt'
+
+result = nr.run(
+    task=netmiko_file_transfer,
+    source_file=test_file,
+    dest_file=test_file,
+    direction='put',
+    num_workers=20,
+)
+std_print(result)
---
+++ send_cmd.py
@@ -0,0 +1,6 @@
+from nornir import InitNornir
+nr = InitNornir("config.yml")
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+result = nr.run(netmiko_send_command, command_string="sh version")
+print_result(result)
